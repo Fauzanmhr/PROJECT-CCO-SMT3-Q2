c6a8b6de28ec7591dbed330de4161de0cf3b74a6
Mohammad Syahdzani Akbar Setyawan <syhdzn1703@gmail.com>
2023-06-15 21:06:53 +0700

    paling baru

--- www/js/gts-login.js
+++ www/js/gts-login.js
@@ -1,23 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
-
 import {
   getAuth,
-  signInWithEmailAndPassword, signOut 
+  signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-
 import {
   getDatabase,
   ref,
   update,
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyChGzVqa5uFCAfJcptW56D8w0AJp1FZEeo",
   authDomain: "growthshop-f2c01.firebaseapp.com",
@@ -33,101 +26,105 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
+//========= START - MEMBUAT FUNGSI UNTUK MELAKUKAN PROSES LOGIN =========\\
 let signinButton = document.getElementById("signin-button");
-
 signinButton.addEventListener("click", (e) => {
-    let emailSignin = document.getElementById("user").value;
-    let passwordSignin = document.getElementById("pass").value;
+  let emailSignin = document.getElementById("user").value;
+  let passwordSignin = document.getElementById("pass").value;
 
+  //========= START - MEMBUAT FUNGSI UNTUK FORM KOSONG =========\\
+  if (emailSignin == "") {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
 
-    if (emailSignin == "") {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
-        })
-        
-        Toast.fire({
-          icon: 'warning',
-          title: 'Input Form Cannot Be Empty'
-        })
-        return false;
-      }
+    Toast.fire({
+      icon: "warning",
+      title: "Input Form Cannot Be Empty",
+    });
+    return false;
+  }
 
-    if (passwordSignin == "") {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
-        })
-        
-        Toast.fire({
-          icon: 'warning',
-          title: 'Input Form Cannot Be Empty'
-        })
-        return false;
-      }
+  if (passwordSignin == "") {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
 
-    signInWithEmailAndPassword(auth, emailSignin, passwordSignin)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        let lgDate = new Date();
-        update(ref(database, "users/" + user.uid), 
-        {last_login: lgDate})
-          .then(() => {
-            // Data saved successfully!
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                didOpen: (toast) => {
-                  toast.addEventListener('mouseenter', Swal.stopTimer)
-                  toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-              })
-              
-              Toast.fire({
-                icon: 'success',
-                title: 'Login Successfully'
-              }).then(function() {
-                window.location = "gts-tambah-barang.html"; // Redirecting to other page.
-            });
-          })
-          .catch((error) => {
-            //the write failed
-            alert(error);
-          });
-      })
-      .catch((error) => {
-        const Toast = Swal.mixin({
+    Toast.fire({
+      icon: "warning",
+      title: "Input Form Cannot Be Empty",
+    });
+    return false;
+  }
+  //========= END - MEMBUAT FUNGSI UNTUK FORM KOSONG =========\\
+
+  //========= START - MEMBUAT FUNGSI SESSION =========\\
+  sessionStorage.setItem("email", emailSignin);
+  //========= END - MEMBUAT FUNGSI SESSION =========\\
+
+  //========= START - MEMBUAT FUNGSI LOGIN DENGAN FIREBASE =========\\
+  signInWithEmailAndPassword(auth, emailSignin, passwordSignin)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      let lgDate = new Date();
+      update(ref(database, "users/" + user.uid), { last_login: lgDate })
+        .then(() => {
+          // Data saved successfully!
+          const Toast = Swal.mixin({
             toast: true,
-            position: 'top-end',
+            position: "top-end",
             showConfirmButton: false,
             timer: 3000,
             didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-          
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+
           Toast.fire({
-            icon: 'error',
-            title: 'The email or password entered is incorrect'
-          })
+            icon: "success",
+            title: "Login Successfully",
+          }).then(function () {
+            window.location = "gts-tambah-barang.html"; // Redirecting to other page.
+          });
+        })
+        .catch((error) => {
+          //the write failed
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        didOpen: (toast) => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
       });
-    signOut(auth)
-      .then(() => {})
-      .catch((error) => {});
-  });
+
+      Toast.fire({
+        icon: "error",
+        title: "The email or password entered is incorrect",
+      });
+    });
+  //========= END - MEMBUAT FUNGSI LOGIN DENGAN FIREBASE =========\\
+});
+//========= END - MEMBUAT FUNGSI UNTUK MELAKUKAN PROSES LOGIN =========\\
